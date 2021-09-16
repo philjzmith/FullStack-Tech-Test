8da11d9a03247abbb9b8e1cdd3198d26459b328e
Philip J Smith <phil@contractsquared.com>
2021-09-16 12:54:12 +0100

    set up jest

--- rick-and-morty/pages/api/intro.ts
+++ rick-and-morty/pages/api/intro.ts
@@ -1,10 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const helloHandler = (req: NextApiRequest, res: NextApiResponse) => {
+const introHandler = (req: NextApiRequest, res: NextApiResponse) => {
   const body: LickApi.IIntro = {
     text: "FE Tech test intro API response"
   }
   res.status(200).json(body)
 }
 
-export default helloHandler;
+export default introHandler;
