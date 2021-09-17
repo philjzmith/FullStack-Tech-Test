38d6a56f85de68c7b56fb018674374c8e420c42c
Philip J Smith <phil@contractsquared.com>
2021-09-17 15:56:35 +0100

    created details page and created details api

--- rick-and-morty/pages/[pageid].tsx
+++ rick-and-morty/pages/[pageid].tsx
@@ -3,14 +3,13 @@ import DetailsTemplate from '@lick/components/templates/Details';
 import { useRouter } from 'next/router'
 import Meta from '@lick/components/shared/Meta';
 
-
 const DetailsPage: NextPage = () => {
   const router = useRouter()
   const { pageid } = router.query;
 
   return <>
     <Meta title="Rick and Morty FE Tech Test" />
-    <DetailsTemplate id={typeof pageid === 'string' ? pageid : pageid[0]} />
+    <DetailsTemplate id={typeof pageid === 'string' ? pageid : ''} />
   </>;
 };
 
